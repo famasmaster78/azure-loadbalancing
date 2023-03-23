e7725d9bb72bc2344127335c8c19f0e0616f7577
jonas <46847021+famasmaster78@users.noreply.github.com>
2023-03-23 14:41:07 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// Dotenv
+require('dotenv').config();
+
+// Express
+const express = require('express');
+const app = express();
+
+// Body Parser
+const bodyParser = require('body-parser');
+
+// OS to get hostname and ip address of the server
+const os = require('os');
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Get all routes
+app.get("*", (req, res) => {
+
+	res.render("index", { 
+		// Render the home page with the data below
+		hostname: JSON.stringify(os.hostname()),
+		ip: JSON.stringify(os.networkInterfaces(), null, 4),
+	});
+});
+
+// Listen to port
+app.listen(process.env.PORT, () => {
+	console.log("Server started on port " + process.env.PORT);
+});

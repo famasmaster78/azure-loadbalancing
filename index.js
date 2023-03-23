89b2ef4699e32c6ba1bca4ba481a1dc6e5bee730
jonas <46847021+famasmaster78@users.noreply.github.com>
2023-03-23 15:05:29 +0100

    updated with mysql connection

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // Dotenv
 require('dotenv').config();
 
+// Mysql
+const mysql = require('mysql2');
+
 // Express
 const express = require('express');
 const app = express();
@@ -18,6 +21,35 @@ app.set('view engine', 'ejs');
 // Get all routes
 app.get("*", (req, res) => {
 
+	// Variable to store data from mysql
+	let data = [];
+
+	// Create mysql connetion
+	const connection = mysql.createConnection({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME
+	});
+
+	// Connect to mysql
+	connection.connect((err) => {
+		if (err) {
+			console.log("Error connecting to database: " + err);
+			return res.status(500).send("Error connecting to database");
+		}
+	});
+
+	// Query the database
+	connection.query("SELECT * FROM users", (err, rows, fields) => {
+		if (err) {
+			console.log("Error querying database: " + err);
+			return res.status(500).send("Error querying database");
+		}
+		// Update data
+		data = rows;
+	});
+
 	// Log incoming request
 	console.log("Incoming request from " + req.ip + " to " + req.originalUrl);
 
@@ -25,6 +57,7 @@ app.get("*", (req, res) => {
 		// Render the home page with the data below
 		hostname: JSON.stringify(os.hostname()),
 		ip: JSON.stringify(os.networkInterfaces(), null, 4),
+		data: JSON.stringify(data, null, 4)
 	});
 });
 
